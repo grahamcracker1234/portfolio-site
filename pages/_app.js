8d79ffd402049a5a75fece7bdda47de12f06ae26
Graham Preston <g.b.preston@proton.me>
2023-01-11 22:39:16 -0500

    Init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
+import { Inter } from '@next/font/google'
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`bg-midnight ${inter.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
