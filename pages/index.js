e25f4cb899931e1ac383529e37af62a11b22e4ee
Graham Preston <g.b.preston@proton.me>
2023-01-13 21:47:01 -0500

    feat: added footer section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 export default function Home({ allPostsData }) {
   return (
@@ -16,6 +17,7 @@ export default function Home({ allPostsData }) {
       <About/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
