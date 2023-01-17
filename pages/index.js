a75398e772093864bbb2b749847c706538d79dae
Graham Preston <g.b.preston@proton.me>
2023-01-17 10:12:27 -0500

    feat: static skills rendering

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,21 @@ import Footer from '../components/Footer';
 import Skills from '../components/Skills';
 import React, { useState } from 'react';
 
-export default function Home({ allPostsData }) {
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+export async function getStaticProps() {
+  const skills = ["Bash", "C#", "CSS", "HTML", "iOS-Development", "Java", "JavaScript", "Node.js", "npm", "PHP", "Python", "R", "Ruby", "Rust", "Sass", "SQL", "Swift", "TypeScript", "Front-End", "Back-End", "Full-Stack", "XML", "Web-Development", "Linux", "macOS", "Microsoft-Excel", "Microsoft-Powerpoint", "Microsoft-Word", "Unity", "Visual-Studio-Code", "Windows", "Xcode", "Next.js", "React", "Tailwind", "Git", "pnpm", "GitHub", "Game-Design", "Search-Engine-Optimization", "User-Experience-Design", "User-Interface-Design", "Adobe-After-Effects", "Adobe-Illustrator", "Adobe-Photoshop", "Adobe-Premier-Pro", "Vercel", "pandas", "NumPy"];
+
+  return await { props: { skills: shuffle(skills) } };
+}
+
+export default function Home({ skills }) {
   const [menuIsActive, setMenuIsActive] = useState(false);
 
   return (
@@ -20,7 +34,7 @@ export default function Home({ allPostsData }) {
       <main className={`${menuIsActive && "blur-md"} filter 200ms ease`}>
         <Header/>
         <About/>
-        <Skills rows={5}/>
+        <Skills rows={5} skills={skills}/>
         <Projects/>
         <Contact/>
         <Footer/>
