0563f377d7e266c13be36b5ff3d95fadebcac5cb
Graham Preston <g.b.preston@proton.me>
2024-06-16 16:25:26 -0400

    fix: grammar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -72,7 +72,7 @@ export default function Projects() {
           ]} />
         <Project 
           name="Pixel3D" 
-          description="A custom render pipeline for Unity that aims to provide a simple and efficient way to render pixel art in realtime-3D."
+          description="A custom render pipeline for Unity that aims to provide a simple and efficient way to render pixel art in real-time 3D."
           technologies={["Unity", "HLSL", "C#", "Open-Source"]} 
           imageSrc="/images/pixel-3d.png" 
           links={[
